8acaefb7f3c1fb930495146a0e1b5be2835ca731
Nick van der Meer <nick.van.der.meer@vanderlande.com>
2023-09-26 10:46:24 +0200

    added first models

---
+++ ClashRoyaleApi/ClashRoyaleApi/Models/RiverRaceLog.cs
@@ -0,0 +1,13 @@
+﻿namespace ClashRoyaleApi.Models
+{
+    public class RiverRaceLog
+    {
+        public int SeasonId { get; set; }
+
+        public int SectionIndex { get; set; }
+
+        public string? CreatedDate { get; set; }
+
+        public List<RiverRaceLogStandings> Standings { get; set; } = new List<RiverRaceLogStandings>(); 
+    }
+}
---
+++ ClashRoyaleApi/ClashRoyaleApi/Models/RiverRaceParticipant.cs
@@ -0,0 +1,17 @@
+﻿namespace ClashRoyaleApi.Models
+{
+    public class RiverRaceParticipant
+    {
+        public string Tag { get; set; }
+
+        public string Name { get; set; }
+
+        public int Fame { get; set; }
+
+        public int RepairPoints { get; set; }
+
+        public int BoatAttacks { get; set; }
+
+        public int DecksUsed { get; set; }
+    }
+}
